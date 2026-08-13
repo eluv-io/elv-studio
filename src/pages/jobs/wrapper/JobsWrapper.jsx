bb199c163c90c2f43146e040908b4412fe660b66
Zenia Villa <zenia.villa@eluv.io>
2026-08-13 12:57:44 -0700

    Load jobs after tenant id is loaded

--- src/pages/jobs/wrapper/JobsWrapper.jsx
+++ src/pages/jobs/wrapper/JobsWrapper.jsx
@@ -1,12 +1,14 @@
 import {useEffect} from "react";
 import {observer} from "mobx-react-lite";
-import {ingestStore} from "@/stores/index.js";
+import {ingestStore, tenantStore} from "@/stores/index.js";
 import {Loader} from "@mantine/core";
 
 const JobsWrapper = observer(({children}) => {
   useEffect(() => {
+    if(!tenantStore.loaded) { return; }
+
     ingestStore.LoadJobs();
-  }, []);
+  }, [tenantStore.loaded]);
 
   if(!ingestStore.jobs) { return <Loader />; }
 
